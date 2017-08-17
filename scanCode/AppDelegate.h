d5589915161f2700a883f0bf02869c903838d5a6
jinshuaier <470401910@qq.com>
2017-08-17 10:37:32 +0800

    Initial Commit

---
+++ scanCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scanCode
+//
+//  Created by 胡高广 on 2017/8/17.
+//  Copyright © 2017年 胡高广. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scanCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scanCode
+//
+//  Created by 胡高广 on 2017/8/17.
+//  Copyright © 2017年 胡高广. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
